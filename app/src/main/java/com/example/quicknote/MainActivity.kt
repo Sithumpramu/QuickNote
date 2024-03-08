39cacc4a3be18e66d5596fdf49897b9848a51f49
Sithum Pramuditha <sithumpramuditha2000@gmail.com>
2024-03-09 04:25:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quicknote/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.quicknote
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        Handler().postDelayed({
+//
+//            startActivity(Intent(this@MainActivity, Onboard::class.java))
+//            finish()
+//        }, 2000)
+
+
+    }
+}
